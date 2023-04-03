6dd8f5d53ac6f30ec0eb4a5840ad60ac6644b537
TruePelmen <oleksiisenyshyn@lnu.edu.ua>
2023-04-03 22:17:51 +0300

    Added List<Vehicle> and tested it

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task2
 {
@@ -31,6 +32,7 @@ namespace Task2
             Console.WriteLine(b.Price_for_trip(200.0, 49));           
             */
 
+
             //Taxi t1 = new Taxi("khjg", 1988, 16433.4, 3, 1000, 150, false, 10000.0);
             //Taxi t2 = new Taxi("khjg", 1988, 16433.4, 3, 1000, 170, false, 10000.0);
             ////Console.WriteLine(t1 == t2);
@@ -55,13 +57,37 @@ namespace Task2
             Console.ReadLine();
             */
 
-            Vehicle vec = new Vehicle("ascd", 1912, 3223.4, 4, 1000, true, 3000.0);
-            Console.WriteLine(vec.CheckTS());
-            vec.Trip(14500);
-            vec.Trip(15000);
-            vec.Trip(500);
-            vec.Trip(500);
-            vec.Trip(500);
+
+
+
+            //testing the event
+            //Vehicle vec = new Vehicle("ascd", 1912, 3223.4, 4, 1000, true, 3000.0);
+            //Console.WriteLine(vec.CheckTS());
+            //vec.Trip(14500);
+            //vec.Trip(15000);
+            //vec.Trip(500);
+            //vec.Trip(500);
+            //vec.Trip(500);
+
+
+
+            //testing reading the taxi from the console
+            //Taxi t = Console.ReadLine();
+            //Console.WriteLine(t);
+
+
+
+            Vehicle a = new Vehicle("ascd", 1912, 3223.4, 4, 1000, true, 3000.0);
+            Taxi t = new Taxi("khjg", 1988, 16433.4, 3, 1000, 150, false, 10000.0);
+            Bus b = new Bus("khjg", 1988, 16433.4, 3, 1000, true, true, 10000.0);
+            List<Vehicle> veh_list = new List<Vehicle>();
+            veh_list.Add(a);
+            veh_list.Add(t);
+            veh_list.Add(b);
+            foreach (Vehicle v in veh_list)
+            {
+                Console.WriteLine(v);
+            }
         }
     }
 }
